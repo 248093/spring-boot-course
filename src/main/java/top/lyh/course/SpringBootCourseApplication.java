55f31df637766d0961b2b3d2f4fb34b52626e8f5
persistence <12574133+dhakljdfs@user.noreply.gitee.com>
2025-09-03 10:45:01 +0800

    featch:SecondComiit

---
+++ src/main/java/top/lyh/course/SpringBootCourseApplication.java
@@ -0,0 +1,20 @@
+package top.lyh.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String index() {
+        return "hello";
+    }
+}
