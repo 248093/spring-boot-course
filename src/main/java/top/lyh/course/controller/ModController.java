5f1faf1848e7fd1e51661df02fd9dd4cb5518575
persistence <12574133+dhakljdfs@user.noreply.gitee.com>
2025-09-05 09:42:46 +0800

    featch:添加了开关功能

--- src/main/java/top/lyh/course/controller/ModController.java
+++ src/main/java/top/lyh/course/controller/ModController.java
@@ -1,2 +1,21 @@
-package top.lyh.course.controller;public class ModController {
+package top.lyh.course.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ModController {
+    @Value("${my.mod}")
+    private String mod;
+    @Value("${my.content}")
+    private String content;
+    @Value("${my.today}")
+    private String today;
+    @Value("${my.author}")
+    private String author;
+    @GetMapping("/mod")
+    public String index() {
+        return "mod: " + mod + " content: " + content + " today: " + today + " author: " + author;
+    }
 }
