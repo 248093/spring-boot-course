72a14b405f66fd51c7be7912be9211580fe81122
persistence <12574133+dhakljdfs@user.noreply.gitee.com>
2025-10-11 17:07:38 +0800

    "fetch:"定时器训练功能"

---
+++ spring-boot-task/src/main/java/top/lyh/mapper/UserAccountMapper.java
@@ -0,0 +1,10 @@
+package top.lyh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.lyh.entity.UserAccount;
+
+@Mapper
+public interface UserAccountMapper extends BaseMapper<UserAccount> {
+    
+}
