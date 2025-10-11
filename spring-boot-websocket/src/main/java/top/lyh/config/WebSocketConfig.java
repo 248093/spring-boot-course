730265a6e4391219e6a0e4531dcec04739b10a21
persistence <12574133+dhakljdfs@user.noreply.gitee.com>
2025-10-11 17:24:03 +0800

    "fetch:"WebSocket功能"

---
+++ spring-boot-websocket/src/main/java/top/lyh/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package top.lyh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import top.lyh.handler.SimpleTimeWebSocketHandler;
+import top.lyh.handler.TestWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // 测试的 WebSocket 端点
+        registry.addHandler(new TestWebSocketHandler(), "/ws/test")
+                .setAllowedOrigins("*");
+        registry.addHandler(new SimpleTimeWebSocketHandler(), "/ws/time").setAllowedOrigins("*");
+    }
+}
